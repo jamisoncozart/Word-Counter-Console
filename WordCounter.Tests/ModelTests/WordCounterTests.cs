c09be94dd847f7b533225fc13f299b8aad07e64b
Jamison Cozart <jamisoncozart@gmail.com>
2020-03-06 08:49:22 -0800

    Add test for Sentence property of RepeatCounter class instance

--- WordCounter.Tests/ModelTests/WordCounterTests.cs
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -13,6 +13,12 @@ namespace WordCounter.Models.Tests
       RepeatCounter newCounter = new RepeatCounter("cat", "This is my cat");
       Assert.AreEqual("cat", newCounter.SearchWord);
     }
+    [TestMethod]
+    public void RepeatCounter_CreatesNewInstanceOfWordCounterWithCorrectSentence_Thisismycat()
+    {
+      RepeatCounter newCounter = new RepeatCounter("cat", "This is my cat");
+      Assert.AreEqual("This is my cat", newCounter.Sentence);
+    }
 
   }
 }
--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -5,6 +5,7 @@ namespace WordCounter.Models
   public class RepeatCounter
   {
     public string SearchWord { get; set; }
+    public string Sentence { get; set; }
     public RepeatCounter(string word, string sentence)
     {
       SearchWord = word;
