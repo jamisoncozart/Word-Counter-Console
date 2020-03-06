703ca1a3246e3e85df6204f9a9e0ab0a88fac3ec
Jamison Cozart <jamisoncozart@gmail.com>
2020-03-06 09:11:54 -0800

    Add test for confirming valid user input

--- WordCounter.Tests/ModelTests/WordCounterTests.cs
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -13,17 +13,27 @@ namespace WordCounter.Models.Tests
       RepeatCounter newCounter = new RepeatCounter("cat", "This is my cat");
       Assert.AreEqual("cat", newCounter.SearchWord);
     }
+
     [TestMethod]
     public void RepeatCounter_CreatesNewInstanceOfWordCounterWithCorrectSentence_Thisismycat()
     {
       RepeatCounter newCounter = new RepeatCounter("cat", "This is my cat");
       Assert.AreEqual("This is my cat", newCounter.Sentence);
     }
+
     [TestMethod]
-    public void IsValidateInput_CorrectlyThrowsErrorWhenNumbersArePresentInUserInput_Error()
+    public void IsValidateInput_CorrectlyReturnsFalseWhenNumbersArePresentInUserInput_false()
     {
       RepeatCounter newCounter = new RepeatCounter("cat1", "This is my cat");
       Assert.AreEqual(false, newCounter.IsValidateInput());
     }
+
+    [TestMethod]
+    public void IsValidateInput_CorrectlyReturnsTrueWhenNumbersArePresentInUserInput_true()
+    {
+      RepeatCounter newCounter = new RepeatCounter("cat", "This is my cat");
+      Assert.AreEqual(true, newCounter.IsValidateInput());
+    }
+
   }
 }
