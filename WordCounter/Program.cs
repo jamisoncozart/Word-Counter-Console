a33e4f770e59b650ef2261ad0a0b22ca06ee9ead
Jamison Cozart <jamisoncozart@gmail.com>
2020-03-06 13:20:55 -0800

    Refactor Program.cs

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -1,41 +1,47 @@
 using System;
 using WordCounter.Models;
+using System.Threading;
 
-namespace WordCounter.Models
+public class Program
 {
-  public class Program
+  public static void Main()
   {
-    public static void Main()
+    Console.Clear();
+    ValidateInputLoop();
+  }
+  
+  public static void ValidateInputLoop()
+  {
+    bool valid = false;
+    while(!valid)
     {
-      Console.Clear();
       RollText("Welcome to Word Counter Console!\n================================\n");
-      bool valid = false;
-      while(!valid)
+      RollText("Please enter a word you would like to search for: ");
+      string searchWord = Console.ReadLine();
+      RollText("Please enter a sentence to search for this word in: ");
+      string searchSentence = Console.ReadLine();
+      RepeatCounter newCounter = new RepeatCounter(searchWord, searchSentence);
+      if(newCounter.IsValidInput())
       {
-        RollText("Please enter a word you would like to search for: ");
-        string searchWord = Console.ReadLine();
-        RollText("Please enter a sentence to search for this word in: ");
-        string searchSentence = Console.ReadLine();
-        RepeatCounter newCounter = new RepeatCounter(searchWord, searchSentence);
-        if(newCounter.IsValidInput())
-        {
-          int instancesOfWord = newCounter.InstancesOfWordInSentence();
-          RollText($"'{searchWord}' is found {instancesOfWord} times in '{searchSentence}'");
-          valid = true;
-        }
-        else
-        {
-          RollText("Invalid input! Please only use letters and punctuation.");
-        }
+        int instancesOfWord = newCounter.InstancesOfWordInSentence();
+        RollText($"'{searchWord}' is found {instancesOfWord} times in '{searchSentence}'");
+        valid = true;
+      }
+      else
+      {
+        RollText("Invalid input! Please only use letters and punctuation.\n");
+        Thread.Sleep(500);
+        Console.Clear();
       }
     }
-    public static void RollText(string str)
+  }
+
+  public static void RollText(string str)
+  {
+    for(int i = 0; i < str.Length; i++)
     {
-      for(int i = 0; i < str.Length; i++)
-      {
-        Console.Write(str[i]);
-        System.Threading.Thread.Sleep(30);
-      }
+      Console.Write(str[i]);
+      Thread.Sleep(30);
     }
   }
 
