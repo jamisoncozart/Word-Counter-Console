55ea6936ddc0acc23b974a7c080c9f89732d299b
Jamison Cozart <jamisoncozart@gmail.com>
2020-03-06 10:37:42 -0800

    Add front-end interface for taking user input and printing to console.

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -7,7 +7,28 @@ namespace WordCounter.Models
   {
     public static void Main()
     {
-     Console.WriteLine();
+      Console.Clear();
+      Console.WriteLine("Welcome to Word Counter Console!\n================================");
+      bool valid = false;
+      while(!valid)
+      {
+        Console.Write("Please enter a word you would like to search for: ");
+        string searchWord = Console.ReadLine();
+        Console.Write("Please enter a sentence to search for this word in: ");
+        string searchSentence = Console.ReadLine();
+        RepeatCounter newCounter = new RepeatCounter(searchWord, searchSentence);
+        if(newCounter.IsValidInput())
+        {
+          int instancesOfWord = newCounter.InstancesOfWordInSentence();
+          Console.WriteLine($"'{searchWord}' is found {instancesOfWord} times in '{searchSentence}'");
+          valid = true;
+        }
+        else
+        {
+          Console.WriteLine("Invalid input! Please only use letters and punctuation.");
+        }
+      }
+
     }
   }
 
